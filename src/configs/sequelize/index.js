31c2d7bb41d8b7d2c871f65daefd2f35175a1343
Hendra Wardana <maxnara19@gmail.com>
2021-02-07 02:44:29 +0700

    add config for dev and prod

--- src/configs/sequelize/index.js
+++ src/configs/sequelize/index.js
@@ -1,14 +1,23 @@
 require('dotenv').config()
 
 module.exports = {
-  dialect: process.env.DB_CONNECTION,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  define: {
-    timestamps: false,
-    freezeTableName: true,
+  development: {
+    dialect: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
   },
 }
