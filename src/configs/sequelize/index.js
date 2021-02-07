6b7d31a26fe88af093d7c912403fed21efa1f67f
Hendra Wardana <maxnara19@gmail.com>
2021-02-07 11:03:57 +0700

    config sequelize

--- src/configs/sequelize/index.js
+++ src/configs/sequelize/index.js
@@ -1,28 +1,14 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-    dialect: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    define: {
-      timestamps: false,
-      freezeTableName: true,
-    },
-  },
-  production: {
-    dialect: process.env.DB_CONNECTION,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    define: {
-      timestamps: false,
-      freezeTableName: true,
-    },
+  dialect: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  define: {
+    timestamps: false,
+    freezeTableName: true,
   },
 }
